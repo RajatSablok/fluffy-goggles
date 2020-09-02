d5c240b60e75148e32b59ecf7a736865589ff0ce
RajatSablok <rajat.main06@gmail.com>
2020-09-02 20:31:36 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const rateLimit = require("express-rate-limit");
+
+require("dotenv").config();
+
+//routers
+
+const app = express();
+
+app.enable("trust proxy");
+
+var limiter = new rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 150,
+  delayMs: 0,
+  message:
+    "Too many requests created from this IP, please try again after 15 minutes",
+});
+
+const DBURI = process.env.DB_URI;
+
+mongoose
+  .connect(DBURI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Database Connected"))
+  .catch((err) => console.log(err));
+
+mongoose.Promise = global.Promise;
+
+app.use(limiter);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Allow CORS
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization, auth-token, On-behalf-of, x-sg-elas-acl"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
+
+app.use((req, res, next) => {
+  const error = new Error("Route not found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
